3b12308896152acc4c10a034e3341350ae32502e
gavin <gavin.gao@e3-expo.com>
2018-07-16 14:05:34 +0800

    修复

--- src/js/main_momo.js
+++ src/js/main_momo.js
@@ -1,13 +1,14 @@
 'use strict'
 $(function () {
   /**
-     * 功能介绍,每个方法请写注释，按下面模板来写.
-     * @author <作者>
-     * @param {类型} 参数名 描述.
-     * @param {string} name=alice 姓名(默认alice).
-     * @param {object} option 配置信息.
-     * @return {Number} 返回值描述.
-     */
+   * 功能介绍,每个方法请写注释，按下面模板来写.
+   * @author <作者>
+   * @param {类型} 参数名 描述.
+   * @param {string} name=alice 姓名(默认alice).
+   * @param {object} option 配置信息.
+   * @return {Number} 返回值描述.
+   */
+
   // 扇形进度图
   var mask = $('.mask')
   mask.each(function () {
