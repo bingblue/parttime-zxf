4145ff94fbb10eb73682b8557f2dbfa2c10bf097
xiaocheng <707996290@qq.com>
2018-07-22 14:54:05 +0800

    个人中心-测试记录
    个人中心-测试记录

--- src/js/main_xc.js
+++ src/js/main_xc.js
@@ -8,5 +8,27 @@ $(function () {
    * @param {object} option 配置信息.
    * @return {Number} 返回值描述.
    */
-
+  var comment = {
+    init: function () {
+      this.testNav()
+    },
+    /**
+     * 个人中心测试记录导航切换
+     * @author xc
+     */
+    testNav: function () {
+      $('#test-nav ul li').on('click', function () {
+        var $li = $(this)
+        var $index = $(this).index()
+        var activeName = 'active'
+        if ($li.hasClass(activeName)) {
+          return false
+        }
+        $li.addClass(activeName).siblings().removeClass(activeName)
+        $li.siblings('.line').css('left', $li.position().left)
+        $('#test-box .test-item').eq($index).addClass(activeName).siblings().removeClass(activeName)
+      })
+    }
+  }
+  comment.init()
 })
