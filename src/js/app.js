420141d0d3c36fa88742da1e088127d56b83bad4
gavin <gavin.gao@e3-expo.com>
2018-11-11 16:28:36 +0800

    udate

--- src/js/app.js
+++ src/js/app.js
@@ -691,13 +691,106 @@
     $html.addClass($.support.touch ? "touch" : "no-touch");
 
 }(jQuery, window, document));
+
+/**
+ * Collapse panels
+ * [data-tool="panel-collapse"]
+ *
+ * Also uses browser storage to keep track
+ * of panels collapsed state
+ */
+(function($, window, document) {
+  'use strict';
+  var panelSelector = '[data-tool="panel-collapse"]',
+      storageKeyName = 'jq-panelState';
+
+  // Prepare the panel to be collapsable and its events
+  $(panelSelector).each(function() {
+    // find the first parent panel
+    var $this        = $(this),
+        parent       = $this.closest('.panel'),
+        wrapper      = parent.find('.panel-wrapper'),
+        collapseOpts = {toggle: false},
+        iconElement  = $this.children('em'),
+        panelId      = parent.attr('id');
+    
+    // if wrapper not added, add it
+    // we need a wrapper to avoid jumping due to the paddings
+    if( ! wrapper.length) {
+      wrapper =
+        parent.children('.panel-heading').nextAll() //find('.panel-body, .panel-footer')
+          .wrapAll('<div/>')
+          .parent()
+          .addClass('panel-wrapper');
+      collapseOpts = {};
+    }
+
+    // Init collapse and bind events to switch icons
+    wrapper
+      .collapse(collapseOpts)
+      .on('hide.bs.collapse', function() {
+        setIconHide( iconElement );
+        savePanelState( panelId, 'hide' );
+        wrapper.prev('.panel-heading').addClass('panel-heading-collapsed');
+      })
+      .on('show.bs.collapse', function() {
+        setIconShow( iconElement );
+        savePanelState( panelId, 'show' );
+        wrapper.prev('.panel-heading').removeClass('panel-heading-collapsed');
+      });
+
+    // Load the saved state if exists
+    var currentState = loadPanelState( panelId );
+    if(currentState) {
+      setTimeout(function() { wrapper.collapse( currentState ); }, 0);
+      savePanelState( panelId, currentState );
+    }
+
+  });
+
+  // finally catch clicks to toggle panel collapse
+  $(document).on('click', panelSelector, function () {
+    
+    var parent = $(this).closest('.panel');
+    var wrapper = parent.find('.panel-wrapper');
+
+    wrapper.collapse('toggle');
+
+  });
+
+  /////////////////////////////////////////////
+  // Common use functions for panel collapse //
+  /////////////////////////////////////////////
+  function setIconShow(iconEl) {
+    iconEl.removeClass('fa-plus').addClass('fa-minus');
+  }
+
+  function setIconHide(iconEl) {
+    iconEl.removeClass('fa-minus').addClass('fa-plus');
+  }
+
+  function savePanelState(id, state) {
+    var data = $.localStorage.get(storageKeyName);
+    if(!data) { data = {}; }
+    data[id] = state;
+    $.localStorage.set(storageKeyName, data);
+  }
+
+  function loadPanelState(id) {
+    var data = $.localStorage.get(storageKeyName);
+    if(data) {
+      return data[id] || false;
+    }
+  }
+
+
+}(jQuery, window, document));
 // Custom jQuery
 // ----------------------------------- 
-
-
 (function(window, document, $, undefined){
 
   $(function(){
+
     $('[data-check-all]').on('change', function() {
       var $this = $(this),
           index= $this.index() + 1,
@@ -706,7 +799,26 @@
       // Make sure to affect only the correct checkbox column
       table.find('tbody > tr > td:nth-child('+index+') input[type="checkbox"]')
         .prop('checked', checkbox[0].checked);
+
     });
+
   });
 
-})(window, document, window.jQuery);
+})(window, document, window.jQuery)
+
+// Morris
+// ----------------------------------- 
+
+
+
+// NAVBAR SEARCH
+// ----------------------------------- 
+
+// (function(window, document, $, undefined){
+
+//   $(function(){
+    
+
+//   });
+
+// })(window, document, window.jQuery);
